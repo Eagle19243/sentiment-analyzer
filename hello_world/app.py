db338c169f8c65886654f0dacd53d5993920c769
Eagle19243 <eagle19243@gmail.com>
2021-03-11 03:26:50 -0500

    ability to use emojis to express sentiment

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,17 +1,14 @@
 import os
 import re
 import nltk
-import en_core_web_sm
 import pandas as pd
-from emoji import get_emoji_regexp
 
 nltk.download('wordnet')
 nltk.download('vader_lexicon')
 nltk.download('punkt')
 
 from nltk.tokenize import RegexpTokenizer, sent_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as SIA
 
 
 def lambda_handler(event, context):
@@ -21,25 +18,15 @@ def lambda_handler(event, context):
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), './cleaned_tickers.csv'))
     tickers = df['ticker'].tolist()
 
-    # Remove emojis if exists
-    text = get_emoji_regexp().sub(u'', text)
+    # Split sentences
     text = re.sub(r'\sand\s|\sor\s', '. ', text)
     tokenized_str = sent_tokenize(text)
-
-    # Remove stop words
-    nlp = en_core_web_sm.load()
-    all_stopwords = nlp.Defaults.stop_words
-    tokens_without_sw = [word for word in tokenized_str if word not in all_stopwords]
-
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_tokens = ([lemmatizer.lemmatize(word) for word in tokens_without_sw])
-    cleaned_output = lemmatized_tokens
 
     # Apply a sentiment analyzer
     sia = SIA()
     result = dict()
 
-    for sentence in cleaned_output:
+    for sentence in tokenized_str:
         pol_score = sia.polarity_scores(sentence)
         tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
         words = tokenizer.tokenize(sentence)
